358899df4bf7d36fb0fb67bf088a6d5a4fee77db
wudixer <steffen.n.lips@gmail.com>
2022-01-07 15:19:14 +0100

    changed session into a trait

--- di/src/registry.rs
+++ di/src/registry.rs
@@ -7,29 +7,32 @@ use traitcast::Castable;
 
 use crate::error::{Error, ErrorCode};
 use crate::service::{Service, ServiceFactory};
-pub struct Session {
-    id: u32,
-}
 
-impl Session {
-    const fn default() -> Session {
-        Session { id: 0 }
+pub trait Session {
+    fn key(&self) -> u32;
+}
+pub struct SimpleSession {
+    key: u32,
+}
+impl SimpleSession {
+    pub const fn default() -> SimpleSession {
+        SimpleSession { key: 0 }
     }
 
-    pub fn new() -> Session {
-        Session {
-            id: SESSION_COUNTER.fetch_add(1, Ordering::Relaxed),
+    pub fn new() -> SimpleSession {
+        SimpleSession {
+            key: SESSION_COUNTER.fetch_add(1, Ordering::Relaxed),
         }
     }
-
-    pub(super) fn id(&self) -> u32 {
-        self.id
+}
+impl Session for SimpleSession {
+    fn key(&self) -> u32 {
+        self.key
     }
 }
 
 static SESSION_COUNTER: AtomicU32 = AtomicU32::new(1);
 
-pub static APPLICATION: Session = Session::default();
 pub struct Registry {
     registered_service_factories: HashMap<String, ServiceFactory>,
     available_sessions: HashMap<u32, HashMap<String, Arc<Mutex<Box<dyn Castable>>>>>,
@@ -89,7 +92,7 @@ impl Registry {
     }
 
     pub fn get_service<Impl: Service + ?Sized>(
-        session: &Session,
+        session: &dyn Session,
     ) -> Result<Arc<Mutex<Box<dyn Castable>>>, Error> {
         let registry = &mut REGISTRY_INSTANCE.lock().or_else(|err| {
             Err(Error::new(
@@ -98,36 +101,36 @@ impl Registry {
             ))
         })?;
 
-        if !registry.available_sessions.contains_key(&session.id()) {
+        if !registry.available_sessions.contains_key(&session.key()) {
             registry
                 .available_sessions
-                .insert(session.id(), HashMap::new());
+                .insert(session.key(), HashMap::new());
         }
 
         let name = std::any::type_name::<Impl>().to_string();
 
-        let service_instance = registry.get_session_service(&session, &name)?;
+        let service_instance = registry.get_session_service(session, &name)?;
 
         Ok(service_instance.clone())
     }
     fn get_session_service(
         &mut self,
-        session: &Session,
+        session: &dyn Session,
         service_name: &String,
     ) -> Result<&Arc<Mutex<Box<dyn Castable>>>, Error> {
-        let session_services = self
-            .available_sessions
-            .get_mut(&session.id())
-            .ok_or_else(|| {
-                Error::new(
-                    ErrorCode::CouldNotCreateSessionService,
-                    format!(
-                        "Could not create session service for session {}",
-                        session.id()
+        let session_services =
+            self.available_sessions
+                .get_mut(&session.key())
+                .ok_or_else(|| {
+                    Error::new(
+                        ErrorCode::CouldNotCreateSessionService,
+                        format!(
+                            "Could not create session service for session {}",
+                            session.key()
+                        )
+                        .as_str(),
                     )
-                    .as_str(),
-                )
-            })?;
+                })?;
 
         if !session_services.contains_key(service_name) {
             let factory = self
@@ -152,7 +155,7 @@ impl Registry {
                 ErrorCode::CouldNotCreateSessionService,
                 format!(
                     "Could not create session service for session {}",
-                    session.id()
+                    session.key()
                 )
                 .as_str(),
             )
--- di/tests/registry.rs
+++ di/tests/registry.rs
@@ -1,6 +1,8 @@
+use std::thread;
+use std::time::Duration;
+
 use di::error::{Error, ErrorCode};
-use di::registry::Registry;
-use di::registry::APPLICATION;
+use di::registry::{Registry, SimpleSession};
 use di::service::Service;
 
 use traitcast::Castable;
@@ -8,14 +10,16 @@ use traitcast_derive::Castable;
 
 trait SimpleService: Service {
     fn foo(&self) -> bool;
-    fn bar(&mut self) -> bool;
+    fn bar(&mut self) -> u32;
 }
 #[derive(Castable)]
 #[Traits(SimpleService)]
-struct SimpleServiceImpl {}
+struct SimpleServiceImpl {
+    counter: u32,
+}
 impl SimpleServiceImpl {
     pub fn factory() -> Box<dyn Castable> {
-        Box::new(SimpleServiceImpl {})
+        Box::new(SimpleServiceImpl { counter: 0 })
     }
 }
 impl SimpleService for SimpleServiceImpl {
@@ -23,8 +27,10 @@ impl SimpleService for SimpleServiceImpl {
         return true;
     }
 
-    fn bar(&mut self) -> bool {
-        return true;
+    fn bar(&mut self) -> u32 {
+        let res = self.counter;
+        self.counter += 1;
+        res
     }
 }
 impl Service for SimpleServiceImpl {}
@@ -42,8 +48,8 @@ impl SimpleService for AnotherSimpleServiceImpl {
         false
     }
 
-    fn bar(&mut self) -> bool {
-        false
+    fn bar(&mut self) -> u32 {
+        0
     }
 }
 impl Service for AnotherSimpleServiceImpl {}
@@ -51,7 +57,7 @@ impl Service for AnotherSimpleServiceImpl {}
 #[test]
 fn get_unregistered_service() {
     assert_eq!(
-        Registry::get_service::<dyn SimpleService>(&APPLICATION).err(),
+        Registry::get_service::<dyn SimpleService>(&SimpleSession::default()).err(),
         Some(Error::new(ErrorCode::UnregisteredService, ""))
     );
 }
@@ -81,21 +87,21 @@ fn unregister_registered_service() {
     Registry::register_service::<dyn SimpleService>(SimpleServiceImpl::factory).ok();
     assert_eq!(Registry::unregister_service::<dyn SimpleService>(), Ok(()));
     assert_eq!(
-        Registry::get_service::<dyn SimpleService>(&APPLICATION).err(),
+        Registry::get_service::<dyn SimpleService>(&SimpleSession::default()).err(),
         Some(Error::new(ErrorCode::UnregisteredService, ""))
     );
 }
 #[test]
 fn create_an_application_service() {
     Registry::register_service::<dyn SimpleService>(SimpleServiceImpl::factory).ok();
-    let service = Registry::get_service::<dyn SimpleService>(&APPLICATION);
+    let service = Registry::get_service::<dyn SimpleService>(&SimpleSession::default());
     assert_eq!(service.is_ok(), true);
     Registry::unregister_service::<dyn SimpleService>().ok();
 }
 #[test]
 fn cast_a_service() {
     Registry::register_service::<dyn SimpleService>(SimpleServiceImpl::factory).ok();
-    let service = Registry::get_service::<dyn SimpleService>(&APPLICATION);
+    let service = Registry::get_service::<dyn SimpleService>(&SimpleSession::default());
     assert_eq!(service.is_ok(), true);
 
     let service = service.unwrap().clone();
@@ -108,13 +114,83 @@ fn cast_a_service() {
 #[test]
 fn cast_a_mutable_service() {
     Registry::register_service::<dyn SimpleService>(SimpleServiceImpl::factory).ok();
-    let service = Registry::get_service::<dyn SimpleService>(&APPLICATION);
+    let service = Registry::get_service::<dyn SimpleService>(&SimpleSession::default());
     assert_eq!(service.is_ok(), true);
 
     let service = service.unwrap().clone();
     let mut service = service.lock().unwrap();
     let service = service.as_mut().query_mut::<dyn SimpleService>();
     assert_eq!(service.is_some(), true);
-    assert_eq!(service.unwrap().bar(), true);
+    assert_eq!(service.unwrap().bar(), 0);
     Registry::unregister_service::<dyn SimpleService>().ok();
+}
+#[test]
+fn get_same_service_in_a_thread() {
+    Registry::register_service::<dyn SimpleService>(SimpleServiceImpl::factory).ok();
+    let service = Registry::get_service::<dyn SimpleService>(&SimpleSession::default());
+
+    let t = thread::spawn(move || {
+        let service = Registry::get_service::<dyn SimpleService>(&SimpleSession::default());
+        let service = service.unwrap().clone();
+
+        thread::sleep(Duration::from_millis(200));
+        {
+            let mut service = service.lock().unwrap();
+            let service = service.as_mut().query_mut::<dyn SimpleService>();
+            assert_eq!(service.is_some(), true);
+            assert_eq!(service.unwrap().bar(), 1);
+        }
+    });
+
+    assert_eq!(service.is_ok(), true);
+    {
+        let service = service.unwrap().clone();
+        let mut service = service.lock().unwrap();
+        let service = service.as_mut().query_mut::<dyn SimpleService>();
+        assert_eq!(service.is_some(), true);
+        assert_eq!(service.unwrap().bar(), 0);
+    }
+    thread::sleep(Duration::from_millis(100));
+
+    Registry::unregister_service::<dyn SimpleService>().ok();
+    assert_eq!(
+        Registry::get_service::<dyn SimpleService>(&SimpleSession::default()).err(),
+        Some(Error::new(ErrorCode::UnregisteredService, ""))
+    );
+    t.join().unwrap();
+}
+#[test]
+fn get_service_for_other_session() {
+    Registry::register_service::<dyn SimpleService>(SimpleServiceImpl::factory).ok();
+    let service = Registry::get_service::<dyn SimpleService>(&SimpleSession::default());
+
+    let t = thread::spawn(move || {
+        let session = SimpleSession::new();
+        let service = Registry::get_service::<dyn SimpleService>(&session);
+        let service = service.unwrap().clone();
+
+        {
+            let mut service = service.lock().unwrap();
+            let service = service.as_mut().query_mut::<dyn SimpleService>();
+            assert_eq!(service.is_some(), true);
+            assert_eq!(service.unwrap().bar(), 0);
+        }
+    });
+
+    assert_eq!(service.is_ok(), true);
+    {
+        let service = service.unwrap().clone();
+        let mut service = service.lock().unwrap();
+        let service = service.as_mut().query_mut::<dyn SimpleService>();
+        assert_eq!(service.is_some(), true);
+        assert_eq!(service.unwrap().bar(), 0);
+    }
+    thread::sleep(Duration::from_millis(100));
+
+    Registry::unregister_service::<dyn SimpleService>().ok();
+    assert_eq!(
+        Registry::get_service::<dyn SimpleService>(&SimpleSession::default()).err(),
+        Some(Error::new(ErrorCode::UnregisteredService, ""))
+    );
+    t.join().unwrap();
 }
